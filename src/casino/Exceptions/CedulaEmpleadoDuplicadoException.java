a2a643e441438d8e885de5a7ef69f80b2418f5aa
carlosalfonso070717-blip <carlosalfonso070717@gmail.com>
2026-03-23 20:11:26 -0600

    Fix typo in exception message for duplicate employee ID

--- src/casino/Exceptions/CedulaEmpleadoDuplicadoException.java
+++ src/casino/Exceptions/CedulaEmpleadoDuplicadoException.java
@@ -3,7 +3,7 @@ package Exceptions;
 public class CedulaEmpleadoDuplicadoException extends Exception{
 private String cedula;
 public CedulaEmpleadoDuplicadoException (String cedula){
-    super("La cedula del empleado ya está registrada: "  + cedula);
+    super("La cédula del empleado ya está registrada: "  + cedula);
     this.cedula = cedula;
 }
     public String getCedula(){
